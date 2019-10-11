f43634fa23f63e6d7d0f077336335673840dc771
whz <whz@123.com>
2019-10-11 17:18:05 +0800

    Initial commit

---
+++ gmall0508-common/src/main/scala/com/atguigu/gmall0508/common/ConstantUtil.scala
@@ -0,0 +1,14 @@
+package com.atguigu.gmall0508.common
+
+/**
+  * @Author 97116
+  * @Date 2019/10/10 20:16
+  * @Version 1.0
+  */
+object ConstantUtil {
+  val STARTUP_TOPIC: String = "startup0508"
+  val EVENT_TOPIC: String = "event0508"
+  val ORDER_TOPIC: String = "order_info0508"
+
+  val ORDER_TABLE_NAME: String = "GMALL0508_ORDER_INFO"
+}
